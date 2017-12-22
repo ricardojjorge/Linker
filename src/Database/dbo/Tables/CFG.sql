a180f7ac14d315921ee5cf3343af45b1e4d51bec
ricardojjorge <33693778+ricardojjorge@users.noreply.github.com>
2017-12-22 16:34:24 +0000

    + column to database

--- src/Database/dbo/Tables/CFG.sql
+++ src/Database/dbo/Tables/CFG.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[CFG] (
     [id]   INT        IDENTITY (1, 1) NOT NULL,
     [name] NCHAR (10) NULL,
-    [code] NCHAR (10) NULL,
+    [code] NCHAR (10) NOT NULL,
+    [description] NCHAR(10) NULL, 
     CONSTRAINT [PK_CFG] PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
