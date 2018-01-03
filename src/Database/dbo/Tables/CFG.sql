9d74aa897241a9cbc447dad4f2f834117fedb21d
ricardojjorge <33693778+ricardojjorge@users.noreply.github.com>
2018-01-03 13:01:38 +0000

    changed to nchar(50)

--- src/Database/dbo/Tables/CFG.sql
+++ src/Database/dbo/Tables/CFG.sql
@@ -2,7 +2,7 @@
     [id]   INT        IDENTITY (1, 1) NOT NULL,
     [name] NCHAR (10) NULL,
     [code] NCHAR (10) NOT NULL,
-    [description] NCHAR(20) NULL, 
+    [description] NCHAR(50) NULL, 
     CONSTRAINT [PK_CFG] PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
