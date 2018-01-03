0f02c277efa7be3d4a768a9ff655f9aba1cec0b3
ricardojjorge <33693778+ricardojjorge@users.noreply.github.com>
2018-01-03 17:30:51 +0000

    add column

--- src/Database/dbo/Tables/CFG.sql
+++ src/Database/dbo/Tables/CFG.sql
@@ -3,6 +3,7 @@
     [name] NCHAR (20) NULL,
     [value] NCHAR (50) NOT NULL,
     [description] NCHAR(256) NOT NULL, 
+    [xpto] NCHAR(10) NULL, 
     CONSTRAINT [PK_CFG] PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
