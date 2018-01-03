2e3f6e1d8232de2d4501dcd9a81f3f9665dfb379
ricardojjorge <33693778+ricardojjorge@users.noreply.github.com>
2018-01-03 11:54:24 +0000

    CFG Table column description changed to nchar(20)

--- src/Database/dbo/Tables/CFG.sql
+++ src/Database/dbo/Tables/CFG.sql
@@ -2,7 +2,7 @@
     [id]   INT        IDENTITY (1, 1) NOT NULL,
     [name] NCHAR (10) NULL,
     [code] NCHAR (10) NOT NULL,
-    [description] NCHAR(10) NULL, 
+    [description] NCHAR(20) NULL, 
     CONSTRAINT [PK_CFG] PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
